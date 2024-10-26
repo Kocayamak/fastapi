8e56e53604024a11e08764b05bfb6958755b03c4
Kocayamak <mikailkocayamak@gmail.com>
2024-10-26 17:30:40 +0300

    redis seed added

--- lib/seed.ts
+++ lib/seed.ts
@@ -258,3 +258,21 @@ const countryList = [
 ];
 
 
+countryList.forEach((country) => {
+    const term = country.toUpperCase();
+    const terms: {score : 0, member: string}[] = [];
+
+    for (let i = 0; i < term.length; i++) {
+        terms.push({score: 0, member: term.substring(0, i)});
+    }
+
+    terms.push({score: 0, member: term});
+
+    const populateDb = async () => {
+        // @ts-expect-error
+        await redis.zadd("terms", ...terms)
+    }
+
+    populateDb();
+})
+
