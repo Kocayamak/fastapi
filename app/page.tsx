1c9e9e0e7ca68e603a218abfff3123ab64b6c0da
Kocayamak <mikailkocayamak@gmail.com>
2024-11-25 22:41:44 +0300

    shadcn command added and landing page completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 
 export default function Home() {
   const [input, setInput] = useState<string>("");
@@ -11,24 +19,67 @@ export default function Home() {
 
   useEffect(() => {
     const fetchData = async () => {
-      if(!input) return setSearchResults(undefined);
+      if (!input) return setSearchResults(undefined);
 
       const res = await fetch(`/api/search?q=${input}`);
 
-      console.log(res)
-    }
+      const data = (await res.json()) as {
+        results: string[];
+        duration: number;
+      };
+
+      setSearchResults(data);
+    };
 
     fetchData();
-  }, [input])
+  }, [input]);
 
   return (
-    <div>
-      <input
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        className="text-zinc-900"
-        type="text"
-      />
-    </div>
+    <main className="h-screen w-screen grainy">
+      <div className="flex flex-col gap-6 items-center pt-32 duration-500 animate-in animate fade-in-5 slide-in-from-bottom-2.5">
+        <h1 className="text-5xl tracking-tight font-bold">SpeedSearch ⚡</h1>
+        <p className="text-zinc-900 text-lg max-w-prose text-center">
+          Hono, Next.js ve CloudFlare ile yüksek performanslı bir API <br />{" "}
+          Aşağıdaki sorguyu çalıştırın ve sonuçlarınızı milisaniye cinsinden
+          alın.
+        </p>
+
+        <div className="max-w-md w-full">
+          <Command>
+            <CommandInput
+              value={input}
+              onValueChange={setInput}
+              placeholder="Ülkeleri arayın..."
+              className="placeholder:text-zinc-500"
+            />
+            <CommandList>
+              {searchResults?.results.length === 0 && (
+                <CommandEmpty>Ülke Bulunamadı.</CommandEmpty>
+              )}
+
+              {searchResults?.results && (
+                <CommandGroup heading="Sonuçlar">
+                  {searchResults.results.map((result, index) => (
+                    <CommandItem key={index} value={result} onSelect={setInput}>
+                      {result}
+                    </CommandItem>
+                  ))}
+                </CommandGroup>
+              )}
+
+              {searchResults?.results && (
+                <>
+                  <div className="h-px w-full bg-zinc-200"></div>
+                  <p className="p-2 text-xs text-zinc-500">
+                    {searchResults?.duration.toFixed(0)} milisaniyede{" "}
+                    {searchResults?.results.length} tane ülke bulundu.
+                  </p>
+                </>
+              )}
+            </CommandList>
+          </Command>
+        </div>
+      </div>
+    </main>
   );
 }
